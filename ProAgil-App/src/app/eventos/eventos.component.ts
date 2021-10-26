1153373c03e1431394f1dd9c58117f228243a9c6
Anderson <ped2.nexsolar@gmail.com>
2021-10-26 18:14:03 -0300

    Enviando parte de CRUD de Eventos.

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,8 +1,17 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { ptBrLocale } from 'ngx-bootstrap/locale';
+defineLocale('pt-br', ptBrLocale);
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Evento } from '../_models/Evento';
 import { EventoService } from '../_services/evento.service';
+import { ThisReceiver } from '@angular/compiler';
+import { throwError } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-eventos',
@@ -13,16 +22,23 @@ export class EventosComponent implements OnInit {
 
   eventosFiltrados: Evento[] = [];
   eventos: Evento[] = [];
+  evento!: Evento;
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
-  modalRef!: BsModalRef; 
+  registerForm!: FormGroup;
+  modoSalvar = 'post'
+  bodyDeletarEvento = ''
 
   _filtroLista!: string;
 
   constructor(private eventoService: EventoService
     ,private modalService : BsModalService
-    ) { }
+    ,private fb: FormBuilder
+    ,private localeService: BsLocaleService  
+    ) {
+      this.localeService.use('pt-br');
+     }
 
   get filtroLista(): string{
     return this._filtroLista;
@@ -33,11 +49,42 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
-  openModal(template : TemplateRef<any>){
-    this.modalRef = this.modalService.show(template);
+  editarEvento(evento: Evento, template : any){
+    this.modoSalvar = 'put'
+    this.evento = evento;
+    this.openModal(template)
+    this.registerForm.patchValue(evento)
+  }
+
+  novoEvento(template : any){
+    this.modoSalvar = 'post'
+    this.openModal(template)
+  }
+
+  excluirEvento(evento: Evento, template: any) {
+    this.openModal(template);
+    this.evento = evento;
+    this.bodyDeletarEvento = `Tem certeza que deseja excluir o Evento: ${evento.tema}, Código: ${evento.id  }`;
+  }
+  
+  confirmeDelete(template: any) {
+    this.eventoService.deleteEvento(this.evento.id).subscribe(
+      () => {
+          template.hide();
+          this.getEventos();
+        }, error => {
+          console.log(error);
+        }
+    );
+  }
+
+  openModal(template : any){
+    this.registerForm.reset();
+    template.show();
   }  
 
   ngOnInit() {
+    this.validation();
     this.getEventos();
   }
 
@@ -50,6 +97,48 @@ export class EventosComponent implements OnInit {
 
   alternarImagem(){
     this.mostrarImagem = !this.mostrarImagem;
+  }
+
+  validation(){
+    this.registerForm = this.fb.group({
+      tema: ['', 
+      [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      imagemUrl: ['', Validators.required],
+      qtdPessoas: ['', [Validators.required, Validators.max(120000)]],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  salvarAlteracao(template: any){
+    if(this.registerForm.valid){
+      if(this.modoSalvar === 'post'){
+        this.evento = Object.assign({}, this.registerForm.value);
+        this.eventoService.postEvento(this.evento).subscribe(
+          (novoEvento: Evento) => {
+            console.log(novoEvento)
+            template.hide();
+            this.getEventos();
+          }, error => {
+            console.log(error);
+          }
+        );
+      }
+      else{
+        this.evento = Object.assign({id: this.evento.id}, this.registerForm.value);
+        this.eventoService.putEvento(this.evento).subscribe(
+          () => {
+            template.hide();
+            this.getEventos();
+          }, error => {
+            console.log(error);
+          }
+        );
+      }
+    }
+
   }
 
   getEventos(){
